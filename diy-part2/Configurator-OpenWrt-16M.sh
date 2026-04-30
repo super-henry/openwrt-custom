d3f6433e3a71a2d260c922cd942d3f6b17e71379
1-1-2 <50569812+1-1-2@users.noreply.github.com>
2026-04-30 15:31:23 +0800

    Remove wget dep from luci-theme-argon Makefile
    uclient-fetch 提供 @wget-any 虚拟包，已经够用，更多的可以靠支持 mbedtls 的 curl ，以拜托因 wget-ssl 带来的对 openssl 的依赖

--- diy-part2/Configurator-OpenWrt-16M.sh
+++ diy-part2/Configurator-OpenWrt-16M.sh
@@ -38,6 +38,10 @@ modification() {
     find -type f -path '*/luci-app-clash/Makefile' -print -exec sed -i 's/openssl/mbedtls/w /dev/stdout' {} \;
 
     echo
+    echo '[MOD] 移除 luci-theme-argon 对 wget 的依赖'
+    find -type f -path '*/luci-theme-argon/Makefile' -print -exec sed -i 's/+wget\b/+wget-any/w /dev/stdout' {} \;
+
+    echo
     echo '[MOD] 更换 luci-app-easymesh 的依赖 openssl 为 mbedtls'
     find -type f -path '*/luci-app-easymesh/Makefile' -print -exec sed -i 's/openssl/mbedtls/w /dev/stdout' {} \;
 
